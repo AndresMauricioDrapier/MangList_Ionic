9a10a3802b6a54dfa454f957d37d3884d53984b3
AndresMaurioDrapier <andresuqui2@gmail.com>
2023-05-18 19:45:21 +0200

    App ionic inicializada

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.manglist_ionic',
+  appName: 'manglist_ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
