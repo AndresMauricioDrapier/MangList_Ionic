3ca8ddf5637058f6f961e1fcce7993f2360e8e09
Néstor Espuch <nestorespuch@gmail.com>
2023-05-22 22:23:54 +0200

    Estilos y card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,10 @@ export class AppComponent {
   menuDisabled = true;
 
   public appPages = [
-    { title: 'Comics Page', url: '/', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Inicio', url: '/', icon: 'home' },
+    { title: 'Categorias', url: '/folder/outbox', icon: 'apps' },
+    { title: 'Contacto', url: '/folder/favorites', icon: 'mail' },
+    { title: 'Sobre nosotros', url: '/folder/archived', icon: 'business' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
